5757b9aa8ef2322c774bb35874723c183fd1cd4f
Paul Paroczai <43162542+pparocza@users.noreply.github.com>
2025-05-21 16:40:07 -0700

    update server script (#57)
    * Mute JackTrip i/o toggle
    
    * Revert "Mute JackTrip i/o toggle"
    
    This reverts commit 819f8937139a21a3af992a3405b6e281d14a5e77.
    
    * Reapply "Mute JackTrip i/o toggle"
    
    This reverts commit 602f9fa5c26d3aa7e2ac0a4898365947eea2976a.
    
    * move and rename folders
    
    * update bpm input
    
    * copy server script from glitch

--- NTM_Transport/glitch/web-server.js
+++ NTM_Transport/glitch/web-server.js
@@ -1,39 +1,50 @@
-import { createServer } from "http";                      
-import { Server } from "socket.io";      
+import { createServer } from "http";
+import { Server } from "socket.io";
 
 const BPM_KEY = "bpm";
-let BPM = 120;
+const BEAT_VALUE_KEY = "beatValue";
 
-let transportDictionary = 
+let BPM = 120;
+let BEAT_VALUE = 4;
+
+let transportDictionary =
 {
-  set BPM(value) { BPM = value; },
-  get BPM() { return BPM; }
+    set BPM(value) { BPM = value; },
+    get BPM() { return BPM; },
+
+    set BeatValue(value) { BEAT_VALUE = value; },
+    get BeatValue() { return BEAT_VALUE; }
 }
 
 
-const httpServer = createServer((req, res) => 
-{    
+const httpServer = createServer((req, res) =>
+{
     res.writeHead(200, {"Content-Type": "text/plain"});
     res.write("Netronome Transport Server Active!");
     console.log("Server Starting!");
     res.end();
 });
 
-const io = new Server(httpServer, 
-{              
+const io = new Server(httpServer,
+{
     cors: { origin: "*" }
 });
 
 
-io.on("connection", (socket) => 
-{   
+io.on("connection", (socket) =>
+{
     socket.emit("initialize", transportDictionary);
-    
-    // Response to client sending BPM_KEY
+
     socket.on(BPM_KEY, (value) =>
     {
         socket.broadcast.emit(BPM_KEY, value);
         transportDictionary.BPM = value;
+    });
+
+    socket.on(BEAT_VALUE_KEY, (value) =>
+    {
+        socket.broadcast.emit(BEAT_VALUE_KEY, value);
+        transportDictionary.BeatValue = value;
     });
 });
 
@@ -41,7 +52,7 @@ io.on("connection", (socket) =>
 // Launch server
 const myPort = process.env.PORT || 3000;
 
-httpServer.listen(myPort, () => 
+httpServer.listen(myPort, () =>
 {
     console.log(`Netronome Transport Server listening on port: ${myPort}`);
 });
