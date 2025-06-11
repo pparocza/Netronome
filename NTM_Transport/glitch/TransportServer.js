66b5ee8d3dc9e4a1557e127f9a529424052efe9c
Paul Paroczai <43162542+pparocza@users.noreply.github.com>
2025-06-11 10:46:28 -0700

    big refactor (#88)
    * Update README.md
    
    * Update README.md
    
    * remove github pages dependency
    
    * Update README.md
    
    * switch to dev server
    
    * restore remote client
    
    * revert server
    
    * Update README.md
    
    * remove github pages dependency
    
    * restore remote client
    
    * TODO
    
    * remove github pages dependency
    
    * restore remote client
    
    * massive refactor
    
    * extract functionality to separate files
    
    * display correct latency measurement value
    
    * update index
    
    * update glitch files

---
+++ NTM_Transport/glitch/TransportServer.js
@@ -0,0 +1,54 @@
+let BPM = 120;
+let BEAT_VALUE = 4;
+let LATENCY_MEASUREMENT_STATUS = false;
+let LATENCY_MEASUREMENT_CLIENT_ID = null;
+
+export const TransportServer =
+{
+	transport:
+		{
+			set bpm(value) { BPM = value; },
+			get bpm() { return BPM; },
+
+			set beatValue(value) { BEAT_VALUE = value; },
+			get beatValue() { return BEAT_VALUE; },
+
+			set latencyMeasurementStatus(value) { LATENCY_MEASUREMENT_STATUS = value; },
+			get latencyMeasurementStatus() { return LATENCY_MEASUREMENT_STATUS; },
+
+			set latencyMeasurementClientId(value) { LATENCY_MEASUREMENT_CLIENT_ID = value; },
+			get latencyMeasurementClientId() { return LATENCY_MEASUREMENT_CLIENT_ID; }
+		},
+
+	key:
+		{
+			bpm: "bpm",
+			beatValue: "beatValue",
+
+			currentUnixTime: "currentUnixTime",
+			requestServerRoundTrip: "requestServerRoundTrip",
+			serverRoundTrip: "serverRoundTrip",
+
+			requestStartLatencyMeasurement: "requestStartLatencyMeasurement",
+			startLatencyMeasurement: "startLatencyMeasurement",
+			requestEndLatencyMeasurement: "requestEndLatencyMeasurement",
+			latencyMeasurementComplete: "latencyMeasurementComplete",
+
+			updateClientList: "updateClientList",
+			removeClient: "removeClient"
+		},
+
+	clients:{},
+
+	addClient(transportClient)
+	{
+		this.clients[transportClient.id.toString()] = transportClient;
+	},
+
+	removeClient(transportClientId)
+	{
+		delete this.clients[transportClientId];
+	},
+
+	nClients() { return Object.keys(this.clients).length; }
+}
---
+++ NTM_Transport/scripts/Display.js
@@ -0,0 +1,51 @@
+/**
+ * Handle display of HTML elements
+ */
+const DISPLAY =
+{
+	_element:
+		{
+			connecting: document.querySelector(".CONNECTING_DISPLAY_CONTENT"),
+			connected: document.querySelector(".CONNECTED_DISPLAY_CONTENT"),
+
+			name: document.querySelector(".NAME_DISPLAY"),
+			nameDiv: document.querySelector(".NAME_DIV"),
+
+			userList: document.querySelector(".USER_LIST_DISPLAY"),
+			userListDiv: document.querySelector(".USER_LIST_DIV"),
+
+			bpm: document.querySelector(".BPM_DISPLAY"),
+			beatValue: document.querySelector(".BEAT_VALUE_DISPLAY"),
+			beatLengthMs: document.querySelector(".BEAT_LENGTH_MS_DISPLAY"),
+
+			unixTime: document.querySelector(".UNIX_TIME_DISPLAY"),
+			serverRoundTrip: document.querySelector(".SERVER_ROUNDTRIP_DISPLAY"),
+
+			latencyMeasurementStatus: document.querySelector(".LATENCY_MEASUREMENT_STATUS_DISPLAY"),
+		},
+
+	get connecting() { return this._element.connecting; },
+	set connecting(value) { this.setInnerHtml(this._element.connecting, value); },
+
+	get connected() { return this._element.connected; },
+	set connected(value) { this.setInnerHtml(this._element.connected, value); },
+
+	set latencyMeasurementStatus(value) { this._element.latencyMeasurementStatus.hidden = !value; },
+
+	set bpm(value) { this.setInnerHtml(this._element.bpm, value); },
+	set beatValue(value) { this.setInnerHtml(this._element.beatValue, value); },
+	set beatLengthMs(value) { this.setInnerHtml(this._element.beatLengthMs, value); },
+
+	set unixTime(value) { this.setInnerHtml(this._element.unixTime, value); },
+	set serverRoundTrip(value) { this.setInnerHtml(this._element.serverRoundTrip, value); },
+
+	get nameDiv() { return this._element.nameDiv; },
+	set nameDisplay(value) { this.setInnerHtml(this._element.name, value); },
+
+	set userList(value) { this.setInnerHtml(this._element.userList, value); },
+
+	setInnerHtml(element, value)
+	{
+		element.innerHTML = value.toString();
+	}
+}
