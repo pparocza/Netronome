b9c8e2da1e48d4257efbd33e1de6459b638ca76c
paulparoczai <paulparoczai@gmail.com>
2025-07-08 11:36:28 -0700

    log server time

--- NTM_M4L/Netronome/Presets/Audio Effects/Max Audio Effect/Imported/NTM_Transport/scripts/socket/Server.js
+++ NTM_M4L/Netronome/Presets/Audio Effects/Max Audio Effect/Imported/NTM_Transport/scripts/socket/Server.js
@@ -89,6 +89,7 @@ function handleCurrentTimeRequest(socket, clientId)
 
 function handleBeatValidationRequest(socket, clientId)
 {
+    console.log("Beat Validation Received At: ", performance.now());
     socket.emit(KEYS.beatValidation, clientId, performance.now());
 }
 
