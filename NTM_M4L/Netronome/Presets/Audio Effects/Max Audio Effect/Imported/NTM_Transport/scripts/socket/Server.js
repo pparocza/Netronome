9eca9f86176cd7d7e14599ed3a55f21c2c96345d
paulparoczai <paulparoczai@gmail.com>
2025-07-08 09:13:44 -0700

    remove server log

--- NTM_M4L/Netronome/Presets/Audio Effects/Max Audio Effect/Imported/NTM_Transport/scripts/socket/Server.js
+++ NTM_M4L/Netronome/Presets/Audio Effects/Max Audio Effect/Imported/NTM_Transport/scripts/socket/Server.js
@@ -89,7 +89,6 @@ function handleCurrentTimeRequest(socket, clientId)
 
 function handleBeatValidationRequest(socket, clientId)
 {
-    console.log(performance.now());
     socket.emit(KEYS.beatValidation, clientId, performance.now());
 }
 
