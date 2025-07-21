81e321871d7055cd6f3952e67063673df126c526
paulparoczai <paulparoczai@gmail.com>
2025-07-21 10:44:28 -0700

    remove setBeathLength from Socket.getServerData()

--- NTM_M4L/Netronome/Presets/Audio Effects/Max Audio Effect/Imported/NTM_Transport/scripts/Socket.js
+++ NTM_M4L/Netronome/Presets/Audio Effects/Max Audio Effect/Imported/NTM_Transport/scripts/Socket.js
@@ -67,7 +67,6 @@ const SOCKET =
 
 		setBPM(SERVER_DATA.transportData.bpm);
 		setBeatValue(SERVER_DATA.transportData.beatValue);
-		// setBeatLength(SERVER_DATA.transportData.beatLengthMs);
 
 		this.requestCurrentServerTime();
 	},
